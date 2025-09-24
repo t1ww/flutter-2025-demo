8ff96161fb3cf660f98589d7f9154cf007e43d9a
t1ww <95153968+t1ww@users.noreply.github.com>
2025-09-24 14:30:03 +0700

    tests

--- mobile_testing/flutter_test_example/test/counter_test.dart
+++ mobile_testing/flutter_test_example/test/counter_test.dart
@@ -9,7 +9,25 @@ void main() {
 
     expect(counter.value, 1);
   });
+  test('Counter value should be incremented by 2', () {
+    final counter = Counter();
 
+    counter.increment();
+    counter.increment();
+
+    expect(counter.value, 2);
+  });
+  test('Counter value should be incremented by 3', () {
+    final counter = Counter();
+
+    counter.increment();
+    counter.increment();
+    counter.increment();
+
+    expect(counter.value, 3);
+  });
+
+  // Decrement
   test('Counter value should be decremented', () {
     final counter = Counter();
 
@@ -17,9 +35,120 @@ void main() {
 
     expect(counter.value, -1);
   });
+  test('Counter value should be decremented by 2', () {
+    final counter = Counter();
+
+    counter.decrement();
+    counter.decrement();
+
+    expect(counter.value, -2);
+  });
+  test('Counter value should be decremented by 3', () {
+    final counter = Counter();
+
+    counter.decrement();
+    counter.decrement();
+    counter.decrement();
+
+    expect(counter.value, -3);
+  });
   test('Counter value should start at zero', () {
     final counter = Counter();
 
     expect(counter.value, 0);
+  });
+
+  // Test counter counter value that should return to zero after increment and decrement.
+  test('Counter value should end at zero', () {
+    final counter = Counter();
+
+    counter.decrement();
+    counter.decrement();
+
+    counter.increment();
+
+    counter.decrement();
+    counter.increment();
+
+    counter.decrement();
+    counter.increment();
+
+    counter.increment();
+
+    expect(counter.value, 0);
+  });
+
+  test('Counter value should end at zero', () {
+    final counter = Counter();
+
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.decrement();
+    counter.increment();
+    counter.increment();
+    counter.decrement();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.increment();
+
+    expect(counter.value, 0);
+  });
+
+  test('Counter value should end at zero', () {
+    final counter = Counter();
+
+    counter.decrement();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.increment();
+
+    expect(counter.value, 0);
+  });
+  // Test cases for Counter value that should handle mixed operations.
+  test('Counter value should end at 1', () {
+    final counter = Counter();
+
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+  test('Counter value should end at 2', () {
+    final counter = Counter();
+
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.decrement();
+    counter.increment();
+    counter.increment();
+
+    expect(counter.value, 2);
   });
 }
